bd57d39b2742bfcd836c5b979cd0286b6283e5ef
Rick Copeland <rick@arborian.com>
2013-10-29 11:52:35 -0400

    Initial commit
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ README.txt
@@ -0,0 +1,1 @@
+dv-list-provider README
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='dv-list-provider',
+      version='0.0',
+      description='dv-list-provider',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="dvlistprovider",
+      entry_points="""\
+      [paste.app_factory]
+      mailchimp = dvlp.mailchimp.main:main
+      salesforce = dvlp.salesforce.main:main
+      constantcontact = dvlp.salesforce.main:main
+      """,
+      )
