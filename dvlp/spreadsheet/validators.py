7b0a5c84fa9393bf59e4313d3bce3b4528e89040
Howard Hamilton <howard@synapp.io>
2014-01-20 16:56:10 -0500

    [#61223014] Simplify file upload/append validation schemas

--- dvlp/spreadsheet/validators.py
+++ dvlp/spreadsheet/validators.py
@@ -11,32 +11,26 @@ class Schema(fes.Schema):
     filter_extra_fields = True
 
 
-class FileByNonce(fev.FancyValidator):
+class FileUpload(fev.NotEmpty):
 
-    def __init__(self, nonce_field='file_nonce', file_field='file', **kwargs):
-        self.nonce_field = nonce_field
-        self.file_field = file_field
-        super(FileByNonce, self).__init__(**kwargs)
+    def to_python(self, value, state=None):
+        if isinstance(value, cgi.FieldStorage):
+            return value
+        else:
+            return super(FileUpload, self).to_python(value, state)
 
-    def validate_python(self, value, state=None):
-        from sfile import model as FM
-        nonce = value[self.nonce_field]
-        f = FM.File.m.find({'metadata.nonce': nonce}).first()
-        if f is None:
-            raise fev.Invalid(
-                'File with nonce <%s> not found' % nonce, value, state)
-        value[self.file_field] = f
+    def from_python(self, value, state=None):
+        return value
+
 
 mapping_schema = Schema(
     header=fev.StringBool(),
     sheet=fev.Int(),
     email=fev.Int())
 
-list_schema = Schema(
-    file_nonce=fev.String(),
-    mapping=mapping_schema,
-    chained_validators=[FileByNonce()])
-
 append_schema = Schema(
-    file_nonce=fev.String(),
-    chained_validators=[FileByNonce()])
+    file=FileUpload())
+
+upload_schema = Schema(
+    file=FileUpload(),
+    mapping=mapping_schema)
